2a93205cab2f13db196bd6b2c97e8a77e7d15d8b
Yuta <ymmt7974@gmail.com>
2021-01-21 15:50:09 +0900

    記事詳細ページの作成

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -18,10 +18,25 @@ class BlogController extends Controller
     public function indexAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
-        $posts = $em->getRepository(Post::class)->findAll();
+        $posts = $em->getRepository(Post::class)->findAll();  // Postを全件取得
 
         return $this->render('blog/index.html.twig', [
             'posts' => $posts,
         ]);
+    }
+
+    /**
+     * @Route("/{id}")
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository(Post::class)->find($id);   //指定されたidのPostを取得
+
+        if (!$post) {
+            throw $this->createNotFoundException('The post does not exist');
+        }
+
+        return $this->render('blog/show.html.twig', ['post' => $post]);
     }
 }
