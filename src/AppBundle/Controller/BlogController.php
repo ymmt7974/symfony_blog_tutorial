b8927ed5c99f436f72103594415e1269721c4dfb
Yuta <ymmt7974@gmail.com>
2021-01-21 14:13:37 +0900

    エンティティ作成

---
+++ src/AppBundle/Controller/BlogController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Post;    # Postエンティティ
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/blog")
+ */
+class BlogController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $posts = $em->getRepository(Post::class)->findAll();
+
+        return $this->render('blog/index.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+}
